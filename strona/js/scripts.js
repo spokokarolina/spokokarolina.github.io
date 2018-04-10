6dd42ec9d1463008f28c49c8824830ef832ef672
spokokarolina <36725882+spokokarolina@users.noreply.github.com>
2018-04-10 13:55:01 +0200

    pliki pliki

---
+++ strona/js/scripts.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function() { 
+   //Strona ladowana jako pierwsza:
+   $('.content').load('pages/home.html');
+ 
+   //Ladowanie pozostalych podstron:
+   $('ul#menu li a').click(function() {
+      var podstrona = $(this).attr('href');
+      $('.content').html('Ładowanie strony...');
+      $('.content').load('pages/'+podstrona);
+      return false;
+   });
+});
